3493cabb9cac7e5d11cb74e787bcfc24e79de20d
André Bauer <monotek@users.noreply.github.com>
2023-05-31 19:42:54 +0200

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,12 +5,14 @@
 
 set -e
 
+SCRIPT_PATH=$(readlink -f "$0")
+
 if [ $# -eq 0  ]; then
     echo "ClamAV Private Database Mirror Updater Cron ${SCRIPT_PATH}"
 
     sudo cron -f
 else
-    echo "ClamAV Private Database Mirror Updater Serve ${SCRIPT_PATH}"
+    echo "ClamAV Private Database Mirror Updater "$@" ${SCRIPT_PATH}"
 
     cvdupdate "$@"
 fi
